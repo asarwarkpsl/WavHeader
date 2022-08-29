07aad6f0f7c294ab9bfdc474e57a22f4805701c2
Azeem Sarwar <asarwar.kpsl@gmail.com>
2022-08-29 13:48:37 +0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WaveHeader
+{
+    public class Programe
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Wav Header");
+        }
+    }
+}
---
+++ WavHeader.cs
@@ -0,0 +1,7 @@
+namespace WaveHeader
+{
+    public class WavHeader
+    {
+        
+    }
+}
